84949f565681a7816dce3fb18f7023e25b5170e8
wchamberlain8 <william.chamberlain8@gmail.com>
2024-08-22 22:22:00 +0100

    Updated tiles to include a width and height in the constructor

--- Frame.java
+++ Frame.java
@@ -11,7 +11,8 @@ public class Frame extends JFrame {
     
     public Frame(){
         setTitle("Outfit Picker");
-        setSize(600, 800);
+        setSize(600, 1000);
+        setBackground(Color.BLACK);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setUpComponents();
     }
@@ -20,6 +21,7 @@ public class Frame extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(3, 1, 0, 5));
         panel.setBackground(Color.BLUE);
+        panel.setPreferredSize(new Dimension(600, 1000));
 
         JPanel topPane = new JPanel();
         topPane.setLayout(new java.awt.FlowLayout());
@@ -32,9 +34,18 @@ public class Frame extends JFrame {
         JPanel bottomPane = new JPanel();
         panel.add(bottomPane);
 
-        Tile newTile = new Tile("HAT");
-        newTile.setBackground(Color.GREEN);
-        topPane.add(newTile);
+        Tile hatTile = new Tile("HAT", 150, 150);
+        hatTile.setBackground(Color.GREEN);
+        topPane.add(hatTile);
+
+        Tile outerwearTile = new Tile("OUTERWEAR", 200, 200);
+        middlePane.add(outerwearTile);
+
+        Tile shirtTile = new Tile("SHIRT", 200, 200);
+        middlePane.add(shirtTile);
+
+        Tile pantsTile = new Tile("PANTS", 150, 200);
+        bottomPane.add(pantsTile);
 
         MenuBar menuBar = new MenuBar();
         setJMenuBar(menuBar);
--- Tile.java
+++ Tile.java
@@ -13,14 +13,19 @@ public class Tile extends JPanel {
 
     private String text;
     private String imagePath;
+    private int width;
+    private int height;
 
     // Constructor to initialize the tile with text and image
-    public Tile(String text) {
+    public Tile(String text, int width, int height) {
         this.text = text;
+        this.width = width;
+        this.height = height;
+
         imagePath = "resources/placeholders/placeholder.jpg";
         
         setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(200, 200));
+        setPreferredSize(new Dimension(width, height));
         
         //Creating the text/title label
         JLabel title = new JLabel(text, SwingConstants.CENTER);
@@ -30,7 +35,7 @@ public class Tile extends JPanel {
         //Creating the image section
         JLabel imageLabel = new JLabel();
         ImageIcon imageIcon = new ImageIcon(imagePath);
-        Image image = imageIcon.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+        Image image = imageIcon.getImage().getScaledInstance(width - 50, height - 50, Image.SCALE_SMOOTH);
         imageLabel.setIcon(new ImageIcon(image));
         imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(imageLabel, BorderLayout.CENTER);
